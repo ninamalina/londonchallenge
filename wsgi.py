efaee425d7283c550ed3a40f21d482d0b35e129e
Greakk <gregor.kuznik@gmail.com>
2016-08-05 18:12:17 +0200

    flask 2

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import os
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+
+from app import app as application
+
+#
+# Below for testing only
+#
